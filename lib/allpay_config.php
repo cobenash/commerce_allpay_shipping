f9d0092358d67f1dc1f81e65504d1232f8d2293b
victor.yang <victor.yang@hellosanta.com.tw>
2019-02-15 12:01:26 +0800

    1st commit

---
+++ lib/allpay_config.php
@@ -0,0 +1,18 @@
+<?php
+	define('ALLPAY_SHIPPING_TEST_MERCHANT_ID', '2000933');
+	define('ALLPAY_SHIPPING_TEST_HASHKEY', 'XBERn1YOvpM9nfZc');
+	define('ALLPAY_SHIPPING_TEST_HASHIV', 'h1ONHk4P4yqbl5LK');
+	define('ALLPAY_NO_OTP_TEST_MERCHANT_ID', '2000214');
+	define('ALLPAY_TEST_SHIPPING_URL', 'https://logistics-stage.ecpay.com.tw/Express/map');
+	define('ALLPAY_SHIPPING_URL', 'https://logistics.ecpay.com.tw/Express/map');
+	define('allpay_cvs_b2c_fami_price',7000);
+	define('allpay_cvs_b2c_unimart_price',7000);
+	define('allpay_cvs_b2c_hilife_price',7000);
+	define('allpay_home_tcat_price',11000);
+	define('allpay_home_tcat_price_extra',11000);
+	define('allpay_home_tcat_price_extra_decimal',110);
+	define('allpay_home_ecan_price',11000);
+	define('allpay_home_ecan_price_extra',11000);
+	define('allpay_home_ecan_price_extra_decimal',110);
+
+?>
